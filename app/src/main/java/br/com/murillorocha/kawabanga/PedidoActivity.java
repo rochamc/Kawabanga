f8160f05b119a00ede96fee91c89cee54725db51
rochamc <murillocrocha@gmail.com>
2016-08-22 22:09:22 -0300

    atualização tela de pedido

--- app/src/main/java/br/com/murillorocha/kawabanga/PedidoActivity.java
+++ app/src/main/java/br/com/murillorocha/kawabanga/PedidoActivity.java
@@ -2,17 +2,44 @@ package br.com.murillorocha.kawabanga;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class PedidoActivity extends AppCompatActivity {
 
     private TextView tvUsuario;
+    private CheckBox cbAtum;
+    private CheckBox cbBacon;
+    private CheckBox cbMussarela;
+    private CheckBox cbCalabresa;
+
+    private RadioGroup rgTamanhoPizza;
+    private Spinner spPagamentos;
+    private CheckBox cbBordaRecheada;
+    private CheckBox cbRecheioExtra;
+    private CheckBox cbRefrigerante;
+    private CheckBox cbSobremesa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido);
         tvUsuario = (TextView)findViewById(R.id.tvUsuario);
 
+        cbAtum = (CheckBox) findViewById(R.id.cbAtum);
+        cbBacon= (CheckBox) findViewById(R.id.cbBacon);
+        cbMussarela= (CheckBox) findViewById(R.id.cbMussarela);
+        cbCalabresa= (CheckBox) findViewById(R.id.cbCalabresa);
+
+        rgTamanhoPizza= (RadioGroup) findViewById(R.id.rgTamanhoPizza);
+        spPagamentos= (Spinner) findViewById(R.id.spPagamentos);
+        cbBordaRecheada= (CheckBox) findViewById(R.id.cbBordaRecheada);
+        cbRecheioExtra= (CheckBox) findViewById(R.id.cbRecheioExtra);
+        cbRefrigerante= (CheckBox) findViewById(R.id.cbRefrigerante);
+        cbSobremesa= (CheckBox) findViewById(R.id.cbSobremesa);
         tvUsuario.setText(getIntent().getStringExtra("usuario"));
     }
 }
