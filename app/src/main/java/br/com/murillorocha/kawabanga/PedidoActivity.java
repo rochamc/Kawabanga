2679e6497c10ddb24a8fd42bd0cffac4c44c0359
rochamc <murillocrocha@gmail.com>
2016-08-22 21:28:32 -0300

    atualização tela de pedido

--- app/src/main/java/br/com/murillorocha/kawabanga/PedidoActivity.java
+++ app/src/main/java/br/com/murillorocha/kawabanga/PedidoActivity.java
@@ -2,12 +2,17 @@ package br.com.murillorocha.kawabanga;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PedidoActivity extends AppCompatActivity {
 
+    private TextView tvUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido);
+        tvUsuario = (TextView)findViewById(R.id.tvUsuario);
+
+        tvUsuario.setText(getIntent().getStringExtra("usuario"));
     }
 }
